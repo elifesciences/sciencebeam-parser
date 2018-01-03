42fc4e83e64f712b68b5a29636336ff7f7f44522
Daniel Ecer <de-code@users.noreply.github.com>
2018-01-03 18:32:19 +0000

    optionally process affiliations via grobid (department and institutions)

--- sciencebeam/transformers/grobid_xml_enhancer.py
+++ sciencebeam/transformers/grobid_xml_enhancer.py
@@ -23,9 +23,19 @@ TEI_SURNAME = TEI_NS_PREFIX + 'surname'
 
 JATS_SURNAME = 'surname'
 JATS_GIVEN_NAMES = 'given-names'
+JATS_ADDR_LINE = 'addr-line'
+JATS_NAMED_CONTENT = 'named-content'
+JATS_INSTITUTION = 'institution'
 
 def get_logger():
   return logging.getLogger(__name__)
+
+def create_or_append(xml_root, path):
+  parent_path, tag_name = rsplit_xml_path(path)
+  parent_node = create_node_recursive(xml_root, parent_path, exists_ok=True)
+  node = E(tag_name)
+  parent_node.append(node)
+  return node
 
 class GrobidXmlEnhancer(object):
   def __init__(self, grobid_url, start_service):
@@ -35,9 +45,14 @@ class GrobidXmlEnhancer(object):
       start_service=start_service,
       field_name='names'
     )
+    self.process_affiliations = grobid_service(
+      grobid_url,
+      GrobidApiPaths.PROCESS_AFFILIATIONS,
+      start_service=start_service,
+      field_name='affiliations'
+    )
 
-  def __call__(self, extracted_xml):
-    xml_root = etree.parse(BytesIO(extracted_xml))
+  def process_and_replace_authors(self, xml_root):
     author_nodes = list(xml_root.findall(XmlPaths.AUTHOR))
     if author_nodes:
       authors = '\n'.join(x.text for x in author_nodes)
@@ -47,15 +62,48 @@ class GrobidXmlEnhancer(object):
       response_xml_root = etree.parse(BytesIO('<dummy>%s</dummy>' % grobid_response))
       for author in author_nodes:
         author.getparent().remove(author)
-      author_parent_path, author_tag_name = rsplit_xml_path(XmlPaths.AUTHOR)
       for pers_name in response_xml_root.findall(TEI_PERS_NAME):
         get_logger().debug('pers_name: %s', pers_name)
-        parent_node = create_node_recursive(xml_root, author_parent_path, exists_ok=True)
-        node = E(author_tag_name)
-        parent_node.append(node)
+        node = create_or_append(xml_root, XmlPaths.AUTHOR)
         for surname in pers_name.findall(TEI_SURNAME):
           node.append(E(JATS_SURNAME, surname.text))
         forenames = [x.text for x in pers_name.findall(TEI_FORNAME)]
         if forenames:
           node.append(E(JATS_GIVEN_NAMES, ' '.join(forenames)))
+    return xml_root
+
+  def process_and_replace_affiliations(self, xml_root):
+    aff_nodes = list(xml_root.findall(XmlPaths.AUTHOR_AFF))
+    if aff_nodes:
+      affiliations = '\n'.join(x.text for x in aff_nodes)
+      get_logger().debug('affiliations: %s', affiliations)
+      grobid_response = self.process_affiliations(affiliations)
+      get_logger().debug('grobid_response: %s', grobid_response)
+      response_xml_root = etree.parse(BytesIO('<dummy>%s</dummy>' % grobid_response))
+      for aff in aff_nodes:
+        aff.getparent().remove(aff)
+      for affiliation in response_xml_root.findall('affiliation'):
+        get_logger().debug('affiliation: %s', affiliation)
+        node = create_or_append(xml_root, XmlPaths.AUTHOR_AFF)
+        for department in affiliation.xpath('./orgName[@type="department"]'):
+          node.append(E(
+            JATS_ADDR_LINE,
+            E(
+              JATS_NAMED_CONTENT,
+              department.text,
+              {
+                'content-type': 'department'
+              }
+            )
+          ))
+        for institution in affiliation.xpath('./orgName[@type="institution"]'):
+          node.append(E(
+            JATS_INSTITUTION,
+            institution.text
+          ))
+
+  def __call__(self, extracted_xml):
+    xml_root = etree.parse(BytesIO(extracted_xml))
+    self.process_and_replace_authors(xml_root)
+    self.process_and_replace_affiliations(xml_root)
     return etree.tostring(xml_root, pretty_print=True)
--- sciencebeam/transformers/grobid_xml_enhancer_test.py
+++ sciencebeam/transformers/grobid_xml_enhancer_test.py
@@ -1,6 +1,7 @@
 import logging
 from io import BytesIO
-from mock import patch
+from contextlib import contextmanager
+from mock import patch, Mock
 
 from lxml import etree
 from lxml.builder import E
@@ -29,6 +30,14 @@ FORENAME_2 = 'forename 2'
 SURNAME_1 = 'surname 1'
 SURNAME_2 = 'surname 2'
 
+DEPARTMENT_1 = 'department 1'
+DEPARTMENT_2 = 'department 2'
+INSTITUTION_1 = 'institution 1'
+INSTITUTION_2 = 'institution 2'
+
+DEPARTMENT_XPATH = './addr-line/named-content[@content-type="department"]'
+INSTITUTION_XPATH = './institution'
+
 def get_logger():
   return logging.getLogger(__name__)
 
@@ -54,23 +63,47 @@ def pers_name(*names):
     surname
   )
 
+def tei_affiliation(department=None, institution=None):
+  affiliation = E.affiliation()
+  if department:
+    affiliation.append(E.orgName(department, type='department'))
+  if institution:
+    affiliation.append(E.orgName(institution, type='institution'))
+  return etree.tostring(affiliation)
+
+def get_text(node):
+  if isinstance(node, list):
+    return ''.join(get_text(x) for x in node)
+  return node.text if node is not None else None
+
 def get_child_text(node, name):
-  child = node.find(name)
-  return child.text if child is not None else None
+  return get_text(node.find(name))
+
+@contextmanager
+def patch_grobid_service():
+  with patch.object(grobid_xml_enhancer, 'grobid_service') as grobid_service:
+    process_header_names = Mock()
+    process_affiliations = Mock()
+    grobid_service.side_effect = [process_header_names, process_affiliations]
+    yield process_header_names, process_affiliations
 
 class TestGrobidXmlEnhancer(object):
   def test_should_initialise_grobid_service(self):
     with patch.object(grobid_xml_enhancer, 'grobid_service') as grobid_service:
       GrobidXmlEnhancer(GROBID_URL, start_service=False)
-      grobid_service.assert_called_with(
+      grobid_service.assert_any_call(
         GROBID_URL, GrobidApiPaths.PROCESS_HEADER_NAMES, start_service=False, field_name='names'
+      )
+      grobid_service.assert_any_call(
+        GROBID_URL, GrobidApiPaths.PROCESS_AFFILIATIONS, start_service=False,
+        field_name='affiliations'
       )
 
   def test_should_convert_single_author(self):
     logging.basicConfig(level='DEBUG')
-    with patch.object(grobid_xml_enhancer, 'grobid_service') as grobid_service:
-      _ = grobid_service
-      grobid_service.return_value.return_value = pers_name(FORENAME_1, SURNAME_1)
+    with patch_grobid_service() as (process_header_names, process_affiliations):
+      _ = process_affiliations
+      process_header_names.return_value = pers_name(FORENAME_1, SURNAME_1)
       enhancer = GrobidXmlEnhancer(GROBID_URL, start_service=False)
       xml_root = E.article()
       create_xml_text(xml_root, XmlPaths.AUTHOR, TEXT_1)
@@ -83,11 +116,33 @@ class TestGrobidXmlEnhancer(object):
         for author in authors
       ] == [(FORENAME_1, SURNAME_1)]
 
+  def test_should_convert_single_affiliation(self):
+    logging.basicConfig(level='DEBUG')
+    with patch_grobid_service() as (process_header_names, process_affiliations):
+      _ = process_header_names
+      process_affiliations.return_value = tei_affiliation(
+        department=DEPARTMENT_1,
+        institution=INSTITUTION_1
+      )
+      enhancer = GrobidXmlEnhancer(GROBID_URL, start_service=False)
+      xml_root = E.article()
+      create_xml_text(xml_root, XmlPaths.AUTHOR_AFF, TEXT_1)
+      enhanced_xml = enhancer(etree.tostring(xml_root))
+      get_logger().info('enhanced_xml: %s', enhanced_xml)
+      enhanced_xml_root = etree.parse(BytesIO(enhanced_xml))
+      affiliations = enhanced_xml_root.findall(XmlPaths.AUTHOR_AFF)
+      assert [
+        get_text(x.xpath(DEPARTMENT_XPATH)) for x in affiliations
+      ] == [DEPARTMENT_1]
+      assert [
+        get_text(x.xpath(INSTITUTION_XPATH)) for x in affiliations
+      ] == [INSTITUTION_1]
+
   def test_should_convert_multiple_author(self):
     logging.basicConfig(level='DEBUG')
-    with patch.object(grobid_xml_enhancer, 'grobid_service') as grobid_service:
-      _ = grobid_service
-      grobid_service.return_value.return_value = (
+    with patch_grobid_service() as (process_header_names, process_affiliations):
+      _ = process_affiliations
+      process_header_names.return_value = (
         pers_name(FORENAME_1, SURNAME_1) +
         pers_name(FORENAME_2, SURNAME_2)
       )
@@ -104,11 +159,39 @@ class TestGrobidXmlEnhancer(object):
         for author in authors
       ] == [(FORENAME_1, SURNAME_1), (FORENAME_2, SURNAME_2)]
 
+  def test_should_convert_multiple_affiliations(self):
+    logging.basicConfig(level='DEBUG')
+    with patch_grobid_service() as (process_header_names, process_affiliations):
+      _ = process_header_names
+      process_affiliations.return_value = (
+        tei_affiliation(
+          department=DEPARTMENT_1,
+          institution=INSTITUTION_1
+        ) +
+        tei_affiliation(
+          department=DEPARTMENT_2,
+          institution=INSTITUTION_2
+        )
+      )
+      enhancer = GrobidXmlEnhancer(GROBID_URL, start_service=False)
+      xml_root = E.article()
+      create_xml_text(xml_root, XmlPaths.AUTHOR_AFF, TEXT_1)
+      enhanced_xml = enhancer(etree.tostring(xml_root))
+      get_logger().info('enhanced_xml: %s', enhanced_xml)
+      enhanced_xml_root = etree.parse(BytesIO(enhanced_xml))
+      affiliations = enhanced_xml_root.findall(XmlPaths.AUTHOR_AFF)
+      assert [
+        get_text(x.xpath(DEPARTMENT_XPATH)) for x in affiliations
+      ] == [DEPARTMENT_1, DEPARTMENT_2]
+      assert [
+        get_text(x.xpath(INSTITUTION_XPATH)) for x in affiliations
+      ] == [INSTITUTION_1, INSTITUTION_2]
+
   def test_should_combine_multiple_forenames(self):
     logging.basicConfig(level='DEBUG')
-    with patch.object(grobid_xml_enhancer, 'grobid_service') as grobid_service:
-      _ = grobid_service
-      grobid_service.return_value.return_value = pers_name(
+    with patch_grobid_service() as (process_header_names, process_affiliations):
+      _ = process_affiliations
+      process_header_names.return_value = pers_name(
         FORENAME_1, FORENAME_2, SURNAME_1
       )
       enhancer = GrobidXmlEnhancer(GROBID_URL, start_service=False)
