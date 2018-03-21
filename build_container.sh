f0bbc54480ef009ee2cb3b124b9798ef64603e8b
Daniel Ecer <de-code@users.noreply.github.com>
2018-03-21 20:44:25 +0000

    changed tag prefix to elifesciences

--- build_container.sh
+++ build_container.sh
@@ -3,4 +3,4 @@ set -e
 
 source .env
 
-docker build -t elife/sciencebeam --build-arg sciencebeam_gym_commit=${SCIENCEBEAM_GYM_COMMIT} .
+docker build -t elifesciences/sciencebeam --build-arg sciencebeam_gym_commit=${SCIENCEBEAM_GYM_COMMIT} .
