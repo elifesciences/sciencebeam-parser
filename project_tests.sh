e67992cb3fdbadf513e4a154b2f49d95ae9783e9
Daniel Ecer <de-code@users.noreply.github.com>
2018-03-22 11:33:45 +0000

    removed no longer needed venv path prefix

--- project_tests.sh
+++ project_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run --rm elifesciences/sciencebeam /bin/bash -c 'venv/bin/pip install -r requirements.dev.txt && venv/bin/pytest sciencebeam'
+docker run --rm elifesciences/sciencebeam /bin/bash -c 'pip install -r requirements.dev.txt && pytest sciencebeam'
