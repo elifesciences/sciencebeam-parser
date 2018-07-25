669712f251ee44880c49fb4ca4219efb502dfd8a
Daniel Ecer <de-code@users.noreply.github.com>
2018-07-25 17:48:43 +0100

    added metypeset pipeline

--- sciencebeam/pipelines/doc_to_docx_pipeline.py
+++ sciencebeam/pipelines/doc_to_docx_pipeline.py
@@ -4,34 +4,34 @@ from sciencebeam_gym.preprocess.preprocessing_utils import change_ext
 
 from sciencebeam.utils.mime_type_constants import MimeTypes
 
-from sciencebeam.transformers.convert_doc import doc_to_pdf
+from sciencebeam.transformers.convert_doc import doc_to_docx
 
 from . import Pipeline, PipelineStep
 
 LOGGER = logging.getLogger(__name__)
 
-class DocToPdfStep(PipelineStep):
+class DocToDocxStep(PipelineStep):
   def get_supported_types(self):
     return {MimeTypes.DOC, MimeTypes.DOCX, MimeTypes.DOTX, MimeTypes.RTF}
 
   def __call__(self, data):
     return {
-      'filename': change_ext(data['filename'], None, '.pdf'),
-      'content': doc_to_pdf(data['content'], data['type']),
-      'type': MimeTypes.PDF
+      'filename': change_ext(data['filename'], None, '.docx'),
+      'content': doc_to_docx(data['content'], data['type']),
+      'type': MimeTypes.DOCX
     }
 
   def __str__(self):
-    return 'DOC to PDF'
+    return 'DOC to DOCX'
 
   def __repr__(self):
     return '%s(%s)' % (type(self).__name__, str(self))
 
-class DocToPdfPipeline(Pipeline):
+class DocToDocxPipeline(Pipeline):
   def add_arguments(self, parser, config, argv=None):
     pass
 
   def get_steps(self, config, args):
-    return [DocToPdfStep()]
+    return [DocToDocxStep()]
 
-PIPELINE = DocToPdfPipeline()
+PIPELINE = DocToDocxPipeline()
