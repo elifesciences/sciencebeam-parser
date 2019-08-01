deae628e4d75eca4c3d8f76a13fa887da4d4d6e1
Daniel Ecer <de-code@users.noreply.github.com>
2019-08-01 11:08:15 +0100

    fixed xslt tests

--- sciencebeam/transformers/xslt.py
+++ sciencebeam/transformers/xslt.py
@@ -1,22 +1,33 @@
-import six
-import lxml.etree as ET
+import logging
+from typing import Union
+
+from lxml import etree
+from lxml.etree import Element, ElementTree
 
 
-def _parse_if_string(x):
-    return ET.fromstring(x) if isinstance(x, six.string_types) else x
+LOGGER = logging.getLogger(__name__)
+
+
+T_XSLT_Input = Union[Element, ElementTree]
+
+
+def _to_xslt_input(value: Union[bytes, str, T_XSLT_Input]) -> T_XSLT_Input:
+    if isinstance(value, (bytes, str)):
+        return etree.fromstring(value)
+    return value
 
 
 def _transform_string_or_dom(transform):
-    return lambda x: transform(_parse_if_string(x))
+    return lambda x: transform(_to_xslt_input(x))
 
 
 def _format_output(root, to_string, pretty_print=True):
-    return ET.tostring(root, pretty_print=pretty_print) if to_string else root
+    return etree.tostring(root, pretty_print=pretty_print) if to_string else root
 
 
 def xslt_transformer_from_file(xslt_filename, *args, **kwargs):
     return xslt_transformer_from_string(
-        ET.tostring(ET.parse(xslt_filename)),
+        etree.tostring(etree.parse(xslt_filename)),
         *args, **kwargs
     )
 
@@ -28,18 +39,20 @@ class xslt_transformer_from_string:
         self.pretty_print = pretty_print
         self.__transform = None
         # validate the XSLT stylesheet
-        ET.fromstring(self.xslt_template)
+        etree.fromstring(self.xslt_template)
 
     def _get_transform(self):
         if self.__transform is None:
             # The transform function cannot be pickled and needs to be loaded lazily
-            transform = ET.XSLT(ET.fromstring(self.xslt_template))
+            transform = etree.XSLT(etree.fromstring(self.xslt_template))
             self.__transform = transform
         return self.__transform
 
     def __call__(self, x):
+        xslt_input = _to_xslt_input(x)
+        LOGGER.debug('xslt_input: %s', xslt_input)
         return _format_output(
-            self._get_transform()(_parse_if_string(x)),
+            self._get_transform()(xslt_input),
             to_string=self.to_string,
             pretty_print=self.pretty_print
         )
