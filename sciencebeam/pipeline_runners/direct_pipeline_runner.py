882896b7586e60a7d9480402bb881cb4384ba8df
Daniel Ecer <de-code@users.noreply.github.com>
2019-08-01 17:21:11 +0100

    simple sequential implementation

--- sciencebeam/pipeline_runners/direct_pipeline_runner.py
+++ sciencebeam/pipeline_runners/direct_pipeline_runner.py
@@ -2,8 +2,27 @@ from __future__ import absolute_import
 
 import argparse
 import logging
+from functools import partial
+from mimetypes import guess_type
+from typing import Callable
+
+from sciencebeam_utils.beam_utils.io import (
+    read_all_from_path,
+    save_file_content
+)
+
+from sciencebeam_utils.utils.file_path import (
+    join_if_relative_path,
+    get_output_file
+)
+
+from sciencebeam_utils.utils.file_list import (
+    load_file_list
+)
 
 from sciencebeam.config.app_config import get_app_config
+
+from sciencebeam.pipelines import Pipeline
 
 from sciencebeam.pipelines import (
     get_pipeline_for_configuration_and_args,
@@ -12,7 +31,13 @@ from sciencebeam.pipelines import (
 
 from sciencebeam.pipeline_runners.beam_pipeline_runner import (
     add_main_args,
-    process_main_args
+    process_main_args,
+    encode_if_text_type,
+    DataProps
+)
+
+from sciencebeam.pipeline_runners.simple_pipeline_runner import (
+    SimplePipelineRunner
 )
 
 
@@ -32,20 +57,65 @@ def parse_args(pipeline, config, argv=None):
 
     process_main_args(args)
 
-    LOGGER.info('args: %s', args)
-
     return args
 
 
-def run(argv=None):
+def load_file_list_for_args(args: argparse.Namespace):
+    file_list_path = join_if_relative_path(args.base_data_path, args.source_file_list)
+    return load_file_list(
+        file_list_path, column=args.source_file_column, limit=args.limit
+    )
+
+
+def get_output_file_for_source_file_fn(args):
+    return partial(
+        get_output_file,
+        source_base_path=args.base_data_path,
+        output_base_path=args.output_path,
+        output_file_suffix=args.output_suffix
+    )
+
+
+def process_file(
+        file_url: str, simple_runner: SimplePipelineRunner,
+        get_output_file_for_source_url: Callable[[str], str]):
+    output_file_url = get_output_file_for_source_url(
+        file_url
+    )
+    file_content = read_all_from_path(file_url)
+    LOGGER.debug('file_content: %s', file_content)
+    data_type = guess_type(file_url)[0]
+    LOGGER.debug('data_type: %s', data_type)
+    result = simple_runner.convert(file_content, file_url, data_type)
+    LOGGER.debug('result: %s', result)
+    save_file_content(
+        output_file_url,
+        encode_if_text_type(result[DataProps.CONTENT])
+    )
+
+
+def run(args, config, pipeline: Pipeline):
+    LOGGER.info('args: %s', args)
+    file_list = load_file_list_for_args(args)
+    LOGGER.debug('file_list: %s', file_list)
+    simple_runner = SimplePipelineRunner(pipeline.get_steps(config, args))
+    for file_url in file_list:
+        process_file(
+            file_url, simple_runner=simple_runner,
+            get_output_file_for_source_url=get_output_file_for_source_file_fn(args)
+        )
+
+
+def main(argv=None):
     config = get_app_config()
 
     pipeline = get_pipeline_for_configuration_and_args(config, argv=argv)
 
-    parse_args(pipeline, config, argv)
+    args = parse_args(pipeline, config, argv)
+    run(args, config=config, pipeline=pipeline)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level='INFO')
 
-    run()
+    main()
---
+++ tests/pipeline_runners/direct_pipeline_runner_test.py
@@ -0,0 +1,45 @@
+import logging
+from pathlib import Path
+
+import pytest
+
+from sciencebeam.utils.mime_type_constants import MimeTypes
+
+from sciencebeam.pipeline_runners.direct_pipeline_runner import (
+    main
+)
+
+from tests.utils.mock_server import MockServer
+
+
+LOGGER = logging.getLogger(__name__)
+
+PDF_CONTENT_1 = b'pdf content 1'
+XML_CONTENT_1 = b'<article>xml content 1</article>'
+
+
+@pytest.mark.slow
+class TestMainEndToEnd:
+    def test_should_convert_single_file(self, temp_dir: Path, mock_server: MockServer):
+        api_url = mock_server.add_response(
+            '/api/convert', XML_CONTENT_1,
+            mimetype=MimeTypes.JATS_XML,
+            methods=('POST',)
+        )
+        LOGGER.debug('api_url: %s', api_url)
+        source_path = temp_dir.joinpath('source')
+        output_path = temp_dir.joinpath('output')
+        source_path.mkdir(parents=True, exist_ok=True)
+        source_path.joinpath('file1.pdf').write_bytes(PDF_CONTENT_1)
+        source_path.joinpath('file-list.tsv').write_text('source_url\nfile1.pdf')
+        output_file = output_path.joinpath('file1.xml')
+        main([
+            '--data-path=%s' % source_path,
+            '--source-file-list=file-list.tsv',
+            '--source-file-column=source_url',
+            '--output-path=%s' % output_path,
+            '--output-suffix=.xml',
+            '--pipeline=api',
+            '--api-url=%s' % api_url
+        ])
+        assert output_file.read_bytes() == XML_CONTENT_1
