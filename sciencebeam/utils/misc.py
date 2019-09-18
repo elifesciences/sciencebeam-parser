47663480f895a73b46398f361cb3e9af11f8e60a
Daniel Ecer <de-code@users.noreply.github.com>
2019-09-18 16:30:38 +0100

    refactored tests to use ApiEndToEndTestHelper

--- sciencebeam/utils/misc.py
+++ sciencebeam/utils/misc.py
@@ -4,6 +4,6 @@ from typing import List
 def dict_to_args(props: dict) -> List[str]:
     return [
         '--%s' % key if isinstance(value, bool) and value
-        else '--%s="%s"' % (key, value)
+        else '--%s=%s' % (key, value)
         for key, value in props.items()
     ]
--- tests/pipeline_runners/local_pipeline_runner_test.py
+++ tests/pipeline_runners/local_pipeline_runner_test.py
@@ -1,10 +1,13 @@
 import logging
 from pathlib import Path
 from unittest.mock import MagicMock
+from typing import List
 
 import pytest
 
 import flask
+
+from sciencebeam_utils.utils.file_list import save_file_list
 
 from sciencebeam.utils.mime_type_constants import MimeTypes
 from sciencebeam.utils.misc import dict_to_args
@@ -37,6 +40,14 @@ DEFAULT_ARGS = {
 DEFAULT_CONFIG = {}
 
 
+OUTPUT_SUFFIX = '.xml'
+
+INPUT_FILE_1 = 'file1.pdf'
+INPUT_FILE_LIST_1 = [INPUT_FILE_1]
+OUTPUT_FILE_1 = 'file1.xml'
+FILE_LIST_FILENAME_1 = 'file-list.tsv'
+
+
 @pytest.fixture(name='pipeline_mock')
 def _pipeline_mock():
     return MagicMock(name='pipeline')
@@ -63,87 +74,128 @@ class TestParseArgs:
         }
 
 
+class ApiEndToEndTestHelper:
+    def __init__(self, temp_dir: Path):
+        self.temp_dir = temp_dir
+        self.source_path = temp_dir.joinpath('source')
+        self.output_path = temp_dir.joinpath('output')
+        self.source_path.mkdir(parents=True, exist_ok=True)
+        self.source_file_list_filename = None
+
+
+    def get_output_file(self, filename: str = OUTPUT_FILE_1) -> Path:
+        return self.output_path.joinpath(filename)
+
+
+    def write_source_file_bytes(
+            self, filename: str = INPUT_FILE_1, data: bytes = PDF_CONTENT_1):
+        self.source_path.joinpath(filename).write_bytes(data)
+
+
+    def write_source_file_list(
+            self,
+            filename: str = 'file-list.tsv',
+            file_list: List[str] = None,
+            column: str = 'source_url'):
+        if file_list is None:
+            file_list = INPUT_FILE_LIST_1
+        self.source_file_list_filename = filename
+        save_file_list(
+            str(self.source_path.joinpath(filename)),
+            file_list=file_list, column=column
+        )
+
+    def write_default_source_files(self):
+        self.write_source_file_bytes()
+        self.write_source_file_list()
+
+    def get_args(self, api_url: str, source_file_list: str = None):
+        args = {
+            'data-path': str(self.source_path),
+            'source-file-column': 'source_url',
+            'output-path': str(self.output_path),
+            'output-suffix': OUTPUT_SUFFIX,
+            'pipeline': 'api',
+            'api-url': api_url
+        }
+        if not source_file_list:
+            source_file_list = self.source_file_list_filename
+        if source_file_list:
+            args['source-file-list'] = source_file_list
+        return args
+
+
+@pytest.fixture(name='api_end_to_end_test_helper')
+def _api_end_to_end_test_helper(temp_dir: Path):
+    return ApiEndToEndTestHelper(temp_dir=temp_dir)
+
+
 @pytest.mark.slow
 class TestMainEndToEnd:
     def test_should_convert_single_file_using_file_list(
-            self, temp_dir: Path, mock_server: MockServer):
+            self, mock_server: MockServer,
+            api_end_to_end_test_helper: ApiEndToEndTestHelper):
         api_url = mock_server.add_response(
             '/api/convert', XML_CONTENT_1,
             mimetype=MimeTypes.JATS_XML,
             methods=('POST',)
         )
         LOGGER.debug('api_url: %s', api_url)
-        source_path = temp_dir.joinpath('source')
-        output_path = temp_dir.joinpath('output')
-        source_path.mkdir(parents=True, exist_ok=True)
-        source_path.joinpath('file1.pdf').write_bytes(PDF_CONTENT_1)
-        source_path.joinpath('file-list.tsv').write_text('source_url\nfile1.pdf')
-        output_file = output_path.joinpath('file1.xml')
-        main([
-            '--data-path=%s' % source_path,
-            '--source-file-list=file-list.tsv',
-            '--source-file-column=source_url',
-            '--output-path=%s' % output_path,
-            '--output-suffix=.xml',
-            '--pipeline=api',
-            '--api-url=%s' % api_url
-        ])
+        api_end_to_end_test_helper.write_source_file_bytes()
+        api_end_to_end_test_helper.write_source_file_list(
+            filename=FILE_LIST_FILENAME_1,
+            file_list=[INPUT_FILE_1],
+            column='source_url'
+        )
+        output_file = api_end_to_end_test_helper.get_output_file()
+        main(dict_to_args(api_end_to_end_test_helper.get_args(
+            api_url=api_url,
+            source_file_list=FILE_LIST_FILENAME_1
+        )))
         assert output_file.read_bytes() == XML_CONTENT_1
 
     def test_should_convert_single_file_using_file_path(
-            self, temp_dir: Path, mock_server: MockServer):
+            self, mock_server: MockServer,
+            api_end_to_end_test_helper: ApiEndToEndTestHelper):
         api_url = mock_server.add_response(
             '/api/convert', XML_CONTENT_1,
             mimetype=MimeTypes.JATS_XML,
             methods=('POST',)
         )
         LOGGER.debug('api_url: %s', api_url)
-        source_path = temp_dir.joinpath('source')
-        output_path = temp_dir.joinpath('output')
-        source_path.mkdir(parents=True, exist_ok=True)
-        source_path.joinpath('file1.pdf').write_bytes(PDF_CONTENT_1)
-        output_file = output_path.joinpath('file1.xml')
-        main([
-            '--data-path=%s' % source_path,
-            '--source-path=*.pdf',
-            '--source-file-column=source_url',
-            '--output-path=%s' % output_path,
-            '--output-suffix=.xml',
-            '--pipeline=api',
-            '--api-url=%s' % api_url
-        ])
+        api_end_to_end_test_helper.write_source_file_bytes()
+        output_file = api_end_to_end_test_helper.get_output_file()
+        main(dict_to_args({
+            **api_end_to_end_test_helper.get_args(
+                api_url=api_url,
+                source_file_list=None
+            ),
+            'source-path': '*.pdf'
+        }))
         assert output_file.read_bytes() == XML_CONTENT_1
 
     def test_should_skip_existing_file(
-            self, temp_dir: Path, mock_server: MockServer):
+            self, mock_server: MockServer,
+            api_end_to_end_test_helper: ApiEndToEndTestHelper):
         api_url = mock_server.add_response(
             '/api/convert', XML_CONTENT_2,
             mimetype=MimeTypes.JATS_XML,
             methods=('POST',)
         )
         LOGGER.debug('api_url: %s', api_url)
-        source_path = temp_dir.joinpath('source')
-        output_path = temp_dir.joinpath('output')
-        source_path.mkdir(parents=True, exist_ok=True)
-        source_path.joinpath('file1.pdf').write_bytes(PDF_CONTENT_1)
-        source_path.joinpath('file-list.tsv').write_text('source_url\nfile1.pdf')
-        output_path.mkdir(parents=True, exist_ok=True)
-        output_file = output_path.joinpath('file1.xml')
+        api_end_to_end_test_helper.write_default_source_files()
+        api_end_to_end_test_helper.output_path.mkdir(parents=True, exist_ok=True)
+        output_file = api_end_to_end_test_helper.get_output_file()
         output_file.write_bytes(XML_CONTENT_1)
-        main([
-            '--data-path=%s' % source_path,
-            '--source-file-list=file-list.tsv',
-            '--source-file-column=source_url',
-            '--output-path=%s' % output_path,
-            '--output-suffix=.xml',
-            '--pipeline=api',
-            '--api-url=%s' % api_url,
-            '--resume'
-        ])
+        main(dict_to_args({
+            **api_end_to_end_test_helper.get_args(api_url=api_url),
+            'resume': True
+        }))
         assert output_file.read_bytes() == XML_CONTENT_1
 
     def test_should_retry_convert_api_call(
-            self, temp_dir: Path, mock_server: MockServer):
+            self, mock_server: MockServer,
+            api_end_to_end_test_helper: ApiEndToEndTestHelper):
         callbacks = [
             lambda: flask.Response('error', status=500),
             lambda: flask.Response(XML_CONTENT_1, mimetype=MimeTypes.JATS_XML),
@@ -153,20 +205,10 @@ class TestMainEndToEnd:
             methods=('POST',)
         )
         LOGGER.debug('api_url: %s', api_url)
-        source_path = temp_dir.joinpath('source')
-        output_path = temp_dir.joinpath('output')
-        source_path.mkdir(parents=True, exist_ok=True)
-        source_path.joinpath('file1.pdf').write_bytes(PDF_CONTENT_1)
-        source_path.joinpath('file-list.tsv').write_text('source_url\nfile1.pdf')
-        output_file = output_path.joinpath('file1.xml')
-        main([
-            '--data-path=%s' % source_path,
-            '--source-file-list=file-list.tsv',
-            '--source-file-column=source_url',
-            '--output-path=%s' % output_path,
-            '--output-suffix=.xml',
-            '--fail-on-error',
-            '--pipeline=api',
-            '--api-url=%s' % api_url
-        ])
+        api_end_to_end_test_helper.write_default_source_files()
+        output_file = api_end_to_end_test_helper.get_output_file()
+        main(dict_to_args({
+            **api_end_to_end_test_helper.get_args(api_url=api_url),
+            'fail-on-error': True
+        }))
         assert output_file.read_bytes() == XML_CONTENT_1
