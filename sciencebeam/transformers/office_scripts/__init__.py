7542f4e4eb50293f94faff6f1a6a1d394dbb6bcd
Daniel Ecer <de-code@users.noreply.github.com>
2019-09-18 18:10:56 +0100

    fallback to using __file__

--- sciencebeam/transformers/office_scripts/__init__.py
+++ sciencebeam/transformers/office_scripts/__init__.py
@@ -3,5 +3,11 @@ import pkg_resources
 
 
 def get_office_script_directory():
-    dist = pkg_resources.get_distribution("sciencebeam")
-    return os.path.join(dist.location, 'sciencebeam/transformers/office_scripts')
+    try:
+        # with Apache Beam, __file__ might refer to the original file
+        # rather than where it is installed to
+        dist = pkg_resources.get_distribution("sciencebeam")
+        return os.path.join(dist.location, 'sciencebeam/transformers/office_scripts')
+    except pkg_resources.DistributionNotFound:
+        # fallback to use __file__
+        return os.path.dirname(__file__)
