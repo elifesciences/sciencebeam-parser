a6c0b929c00ee37b10e2f7998e13ea4529bbb03a
Daniel Ecer <de-code@users.noreply.github.com>
2021-05-23 11:24:14 +0100

    clean abstract (#32)

--- pygrobid/models/header/model.py
+++ pygrobid/models/header/model.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from typing import Iterable, Tuple
 
 from pygrobid.document.tei_document import TeiDocument
@@ -7,6 +8,21 @@ from pygrobid.models.delft_model import DelftModel
 
 
 LOGGER = logging.getLogger(__name__)
+
+
+# based on:
+#   grobid-core/src/main/java/org/grobid/core/data/BiblioItem.java
+ABSTRACT_REGEX = r'^(?:(?:abstract|summary|résumé|abrégé|a b s t r a c t)(?:[.:])?)?\s*(.*)'
+
+
+def get_cleaned_abstract_text(text: str) -> str:
+    if not text:
+        return text
+    m = re.match(ABSTRACT_REGEX, text, re.IGNORECASE)
+    if not m:
+        LOGGER.debug('text does not match regex: %r', text)
+        return text
+    return m.group(1)
 
 
 class HeaderModel(DelftModel):
@@ -27,5 +43,6 @@ class HeaderModel(DelftModel):
                 current_title = value
                 document.set_title(value)
             if name == '<abstract>' and not current_abstract:
+                value = get_cleaned_abstract_text(value)
                 current_abstract = value
                 document.set_abstract(value)
--- tests/models/header/model_test.py
+++ tests/models/header/model_test.py
@@ -1,9 +1,38 @@
-from pygrobid.models.header.model import HeaderModel
+from pygrobid.models.header.model import (
+    HeaderModel,
+    get_cleaned_abstract_text
+)
 from pygrobid.document.tei_document import TeiDocument
 
 
 TITLE_1 = 'the title 1'
 ABSTRACT_1 = 'the abstract 1'
+
+
+class TestGetCleanedAbstractText:
+    def test_should_return_none_if_passed_in_text_was_none(self):
+        assert get_cleaned_abstract_text(None) is None
+
+    def test_should_return_empty_str_if_passed_in_text_was_empty(self):
+        assert get_cleaned_abstract_text('') == ''
+
+    def test_should_return_abstract_if_it_doesnt_contain_prefix(self):
+        assert get_cleaned_abstract_text(ABSTRACT_1) == ABSTRACT_1
+
+    def test_should_return_remove_abstract_prefix(self):
+        assert get_cleaned_abstract_text(
+            'Abstract ' + ABSTRACT_1
+        ) == ABSTRACT_1
+
+    def test_should_return_remove_abstract_dot_prefix(self):
+        assert get_cleaned_abstract_text(
+            'Abstract. ' + ABSTRACT_1
+        ) == ABSTRACT_1
+
+    def test_should_return_remove_abstract_colon_prefix(self):
+        assert get_cleaned_abstract_text(
+            'Abstract: ' + ABSTRACT_1
+        ) == ABSTRACT_1
 
 
 class TestHeaderModel:
