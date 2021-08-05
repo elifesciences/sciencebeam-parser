6ce90b74681f6c17c1a22a95cbaf9943cd94cc47
Daniel Ecer <de-code@users.noreply.github.com>
2021-08-05 20:25:15 +0100

    add author aff markers as delimters (#112)

--- sciencebeam_parser/utils/tokenizer.py
+++ sciencebeam_parser/utils/tokenizer.py
@@ -4,8 +4,14 @@ from typing import Iterable, List
 
 # delimters mostly copied from:
 # https://github.com/kermitt2/delft/blob/v0.2.6/delft/utilities/Tokenizer.py
-# added: `@`, `#`
-DELIMITERS = "\n\r\t\f\u00A0([ •*,:;?.!/#)-−–‐\"“”‘’'`$]*\u2666\u2665\u2663\u2660\u00A0@"
+# and:
+# https://github.com/kermitt2/grobid/blob/0.6.2/grobid-core/src/main/java/org/grobid/core/utilities/TextUtilities.java#L773-L948
+# added: `@`, `#`, `\u2020`, ...
+_COMMON_AFF_MARKERS = '\u2020\u2021\u00A7\u00B6\u204B\u01C2'
+DELIMITERS = (
+    "\n\r\t\f\u00A0([ •*,:;?.!/#)-−–‐\"“”‘’'`$]*\u2666\u2665\u2663\u2660\u00A0@"
+    + _COMMON_AFF_MARKERS
+)
 DELIMITERS_REGEX = r'(' + r'|'.join(map(re.escape, DELIMITERS)) + r'|\s)'
 
 
