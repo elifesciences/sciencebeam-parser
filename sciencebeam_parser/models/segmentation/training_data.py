9c8830cd42b3a0d07e57276e745f449453dcb46b
Daniel Ecer <de-code@users.noreply.github.com>
2021-11-25 20:13:30 +0000

    refactored iter_tei_child_for_model_data_iterable

--- sciencebeam_parser/models/segmentation/training_data.py
+++ sciencebeam_parser/models/segmentation/training_data.py
@@ -6,6 +6,7 @@ from lxml.builder import ElementMaker
 
 from sciencebeam_parser.document.layout_document import (
     LayoutDocument,
+    LayoutLine,
     LayoutToken,
     join_layout_tokens
 )
@@ -26,6 +27,27 @@ def iter_tokens_from_model_data(model_data: LayoutModelData) -> Iterable[LayoutT
     yield from model_data.layout_line.tokens
 
 
+def iter_layout_lines_from_layout_tokens(
+    layout_tokens: Iterable[LayoutToken]
+) -> Iterable[LayoutLine]:
+    line_layout_tokens: List[LayoutToken] = []
+    for layout_token in layout_tokens:
+        if not line_layout_tokens:
+            line_layout_tokens.append(layout_token)
+            continue
+        if (
+            layout_token.line_descriptor.line_id
+            == line_layout_tokens[0].line_descriptor.line_id
+        ):
+            LOGGER.debug('line id matching: %r - %r', layout_token, line_layout_tokens[0])
+            line_layout_tokens.append(layout_token)
+            continue
+        yield LayoutLine(tokens=line_layout_tokens)
+        line_layout_tokens = [layout_token]
+    if line_layout_tokens:
+        yield LayoutLine(tokens=line_layout_tokens)
+
+
 class SegmentationTeiTrainingDataGenerator:
     def iter_training_tei_children_for_line_layout_tokens(
         self,
@@ -35,40 +57,25 @@ class SegmentationTeiTrainingDataGenerator:
         yield TEI_E.lb()
         yield '\n'
 
-    def iter_tei_child_for_tokens(
+    def iter_training_tei_children_for_line_layout_lines(
         self,
-        layout_tokens: Iterable[LayoutToken]
+        layout_lines: Iterable[LayoutLine]
     ) -> Iterable[Union[str, etree.ElementBase]]:
-        line_layout_tokens: List[LayoutToken] = []
-        for layout_token in layout_tokens:
-            if not line_layout_tokens:
-                line_layout_tokens.append(layout_token)
-                continue
-            if (
-                layout_token.line_descriptor.line_id
-                == line_layout_tokens[0].line_descriptor.line_id
-            ):
-                LOGGER.debug('line id matching: %r - %r', layout_token, line_layout_tokens[0])
-                line_layout_tokens.append(layout_token)
-                continue
+        for layout_line in layout_lines:
             yield from self.iter_training_tei_children_for_line_layout_tokens(
-                line_layout_tokens
-            )
-            line_layout_tokens.clear()
-            line_layout_tokens.append(layout_token)
-        if line_layout_tokens:
-            yield from self.iter_training_tei_children_for_line_layout_tokens(
-                line_layout_tokens
+                layout_line.tokens
             )
 
     def iter_tei_child_for_model_data_iterable(
         self,
         model_data_iterable: Iterable[LayoutModelData]
     ) -> Iterable[Union[str, etree.ElementBase]]:
-        yield from self.iter_tei_child_for_tokens((
-            layout_token
+        yield from self.iter_training_tei_children_for_line_layout_lines((
+            layout_line
             for model_data in model_data_iterable
-            for layout_token in iter_tokens_from_model_data(model_data)
+            for layout_line in iter_layout_lines_from_layout_tokens(
+                iter_tokens_from_model_data(model_data)
+            )
         ))
 
     def get_training_tei_xml_for_model_data_iterable(
