bec074fd157c5ed36344fe6bea65a9d61b8d09bd
Daniel Ecer <de-code@users.noreply.github.com>
2021-11-25 16:47:41 +0000

    added test_should_add_line_feeds

--- tests/models/segmentation/training_data_test.py
+++ tests/models/segmentation/training_data_test.py
@@ -1,4 +1,8 @@
-from sciencebeam_parser.document.layout_document import LayoutBlock, LayoutDocument
+from sciencebeam_parser.document.layout_document import (
+    LayoutBlock,
+    LayoutDocument,
+    LayoutLine
+)
 from sciencebeam_parser.models.segmentation.training_data import (
     SegmentationTeiTrainingDataGenerator
 )
@@ -6,6 +10,7 @@ from sciencebeam_parser.utils.xml import get_text_content
 
 
 TEXT_1 = 'this is text 1'
+TEXT_2 = 'this is text 2'
 
 
 class TestSegmentationTeiTrainingDataGenerator:
@@ -18,3 +23,16 @@ class TestSegmentationTeiTrainingDataGenerator:
         text_nodes = xml_root.xpath('./text')
         assert len(text_nodes) == 1
         assert get_text_content(text_nodes[0]) == TEXT_1
+
+    def test_should_add_line_feeds(self):
+        training_data_generator = SegmentationTeiTrainingDataGenerator()
+        layout_document = LayoutDocument.for_blocks([LayoutBlock(lines=[
+            LayoutLine.for_text(TEXT_1, tail_whitespace='\n'),
+            LayoutLine.for_text(TEXT_2, tail_whitespace='\n')
+        ])])
+        xml_root = training_data_generator.get_training_tei_xml_for_layout_document(
+            layout_document
+        )
+        text_nodes = xml_root.xpath('./text')
+        assert len(text_nodes) == 1
+        assert get_text_content(text_nodes[0]) == '\n'.join([TEXT_1, TEXT_2])
