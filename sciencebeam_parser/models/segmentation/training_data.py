b85c9eaecab9045d7867ba6af3496c1e1d6cb63c
Daniel Ecer <de-code@users.noreply.github.com>
2021-11-25 20:02:58 +0000

    added get_training_tei_xml_for_model_data_iterable

--- sciencebeam_parser/models/segmentation/training_data.py
+++ sciencebeam_parser/models/segmentation/training_data.py
@@ -1,14 +1,41 @@
-from typing import Iterable, Union
+import logging
+from typing import Iterable, List, Union
+
 from lxml import etree
 from lxml.builder import ElementMaker
 
-from sciencebeam_parser.document.layout_document import LayoutDocument, LayoutLine
+from sciencebeam_parser.document.layout_document import (
+    LayoutDocument,
+    LayoutLine,
+    LayoutToken,
+    join_layout_tokens
+)
+from sciencebeam_parser.models.data import LayoutModelData
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 TEI_E = ElementMaker()
 
 
+def iter_tokens_from_model_data(model_data: LayoutModelData) -> Iterable[LayoutToken]:
+    if model_data.layout_token is not None:
+        yield model_data.layout_token
+        return
+    assert model_data.layout_line is not None
+    yield from model_data.layout_line.tokens
+
+
 class SegmentationTeiTrainingDataGenerator:
+    def iter_training_tei_children_for_line_layout_tokens(
+        self,
+        layout_tokens: Iterable[LayoutToken]
+    ) -> Iterable[Union[str, etree.ElementBase]]:
+        yield join_layout_tokens(layout_tokens)
+        yield TEI_E.lb()
+        yield '\n'
+
     def get_training_tei_children_for_layout_line(
         self,
         layout_line: LayoutLine
@@ -16,6 +43,52 @@ class SegmentationTeiTrainingDataGenerator:
         yield layout_line.text
         yield TEI_E.lb()
         yield '\n'
+
+    def iter_tei_child_for_tokens(
+        self,
+        layout_tokens: Iterable[LayoutToken]
+    ) -> Iterable[Union[str, etree.ElementBase]]:
+        line_layout_tokens: List[LayoutToken] = []
+        for layout_token in layout_tokens:
+            if not line_layout_tokens:
+                line_layout_tokens.append(layout_token)
+                continue
+            if (
+                layout_token.line_descriptor.line_id
+                == line_layout_tokens[0].line_descriptor.line_id
+            ):
+                LOGGER.debug('line id matching: %r - %r', layout_token, line_layout_tokens[0])
+                line_layout_tokens.append(layout_token)
+                continue
+            yield from self.iter_training_tei_children_for_line_layout_tokens(
+                line_layout_tokens
+            )
+            line_layout_tokens.clear()
+            line_layout_tokens.append(layout_token)
+        if line_layout_tokens:
+            yield from self.iter_training_tei_children_for_line_layout_tokens(
+                line_layout_tokens
+            )
+
+    def iter_tei_child_for_model_data_iterable(
+        self,
+        model_data_iterable: Iterable[LayoutModelData]
+    ) -> Iterable[Union[str, etree.ElementBase]]:
+        yield from self.iter_tei_child_for_tokens((
+            layout_token
+            for model_data in model_data_iterable
+            for layout_token in iter_tokens_from_model_data(model_data)
+        ))
+
+    def get_training_tei_xml_for_model_data_iterable(
+        self,
+        model_data_iterable: Iterable[LayoutModelData]
+    ) -> etree.ElementBase:
+        return TEI_E.tei(
+            TEI_E.text(*list(
+                self.iter_tei_child_for_model_data_iterable(model_data_iterable)
+            ))
+        )
 
     def get_training_tei_xml_for_layout_document(
         self,
--- tests/models/segmentation/training_data_test.py
+++ tests/models/segmentation/training_data_test.py
@@ -1,12 +1,22 @@
+import logging
+
+from lxml import etree
+
 from sciencebeam_parser.document.layout_document import (
     LayoutBlock,
     LayoutDocument,
-    LayoutLine
+    LayoutLine,
+    LayoutLineDescriptor
 )
+from sciencebeam_parser.models.data import DEFAULT_DOCUMENT_FEATURES_CONTEXT
+from sciencebeam_parser.models.segmentation.data import SegmentationDataGenerator
 from sciencebeam_parser.models.segmentation.training_data import (
     SegmentationTeiTrainingDataGenerator
 )
 from sciencebeam_parser.utils.xml import get_text_content
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 TEXT_1 = 'this is text 1'
@@ -46,6 +56,38 @@ class TestSegmentationTeiTrainingDataGenerator:
         xml_root = training_data_generator.get_training_tei_xml_for_layout_document(
             layout_document
         )
+        text_nodes = xml_root.xpath('./text')
+        assert len(text_nodes) == 1
+        lb_nodes = text_nodes[0].xpath('lb')
+        assert len(lb_nodes) == 2
+        assert lb_nodes[0].getparent().text == TEXT_1
+        assert lb_nodes[0].tail == '\n' + TEXT_2
+
+    def test_should_generate_tei_from_model_data(self):
+        layout_document = LayoutDocument.for_blocks([LayoutBlock(lines=[
+            LayoutLine.for_text(
+                TEXT_1,
+                tail_whitespace='\n',
+                line_descriptor=LayoutLineDescriptor(line_id=1)
+            ),
+            LayoutLine.for_text(
+                TEXT_2,
+                tail_whitespace='\n',
+                line_descriptor=LayoutLineDescriptor(line_id=2)
+            )
+        ])])
+        data_generator = SegmentationDataGenerator(
+            DEFAULT_DOCUMENT_FEATURES_CONTEXT,
+            use_first_token_of_block=True
+        )
+        model_data_iterable = data_generator.iter_model_data_for_layout_document(
+            layout_document
+        )
+        training_data_generator = SegmentationTeiTrainingDataGenerator()
+        xml_root = training_data_generator.get_training_tei_xml_for_model_data_iterable(
+            model_data_iterable
+        )
+        LOGGER.debug('xml: %r', etree.tostring(xml_root))
         text_nodes = xml_root.xpath('./text')
         assert len(text_nodes) == 1
         lb_nodes = text_nodes[0].xpath('lb')
