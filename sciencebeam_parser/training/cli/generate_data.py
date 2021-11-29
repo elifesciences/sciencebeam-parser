71b8a1246b2760ed39b4fc1ae988010ad60f8f72
Daniel Ecer <de-code@users.noreply.github.com>
2021-11-29 19:52:37 +0000

    generate output path

--- sciencebeam_parser/training/cli/generate_data.py
+++ sciencebeam_parser/training/cli/generate_data.py
@@ -1,12 +1,45 @@
+import argparse
 import logging
+from pathlib import Path
 from typing import List, Optional
 
 
 LOGGER = logging.getLogger(__name__)
 
 
+def parse_args(argv: Optional[List[str]] = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        'ScienceBeam Parser: Generate Training Data'
+    )
+    parser.add_argument(
+        '--model',
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        '--source-path',
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        '--output-path',
+        type=str,
+        required=True
+    )
+    return parser.parse_args(argv)
+
+
+def run(args: argparse.Namespace):
+    LOGGER.info('args: %r', args)
+    output_path = Path(args.output_path)
+    LOGGER.info('output_path: %r', output_path)
+    output_path.mkdir(parents=True, exist_ok=True)
+
+
 def main(argv: Optional[List[str]] = None):
-    LOGGER.info('argv: %r', argv)
+    LOGGER.debug('argv: %r', argv)
+    args = parse_args(argv)
+    run(args)
 
 
 if __name__ == '__main__':
--- tests/training/cli/generate_data_test.py
+++ tests/training/cli/generate_data_test.py
@@ -1,12 +1,19 @@
+from pathlib import Path
+
 from sciencebeam_parser.training.cli.generate_data import (
     main
 )
 
 
 class TestMain:
-    def test_should_be_able_to_generate_segmentation_training_data(self):
+    def test_should_be_able_to_generate_segmentation_training_data(
+        self,
+        tmp_path: Path
+    ):
+        output_path = tmp_path / 'generated-data'
         main([
             '--model="segmentation"',
             '--source-path="test-data/minimal-example.pdf"',
-            '--output-path="./data/generated-training-data"'
+            f'--output-path={output_path}'
         ])
+        assert output_path.exists()
