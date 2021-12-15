4c4fcc5f8c797ade73d2d51b522f18daf68cb579
Daniel Ecer <de-code@users.noreply.github.com>
2021-12-15 21:48:29 +0000

    extracted ReferenceSegmenterModelTrainingDataGenerator

--- sciencebeam_parser/models/reference_segmenter/training_data.py
+++ sciencebeam_parser/models/reference_segmenter/training_data.py
@@ -33,5 +33,11 @@ class ReferenceSegmenterTeiTrainingDataGenerator(AbstractTeiTrainingDataGenerato
             root_training_xml_element_path=ROOT_TRAINING_XML_ELEMENT_PATH,
             training_xml_element_path_by_label=TRAINING_XML_ELEMENT_PATH_BY_LABEL,
             reset_training_xml_element_path_by_label=RESET_TRAINING_XML_ELEMENT_PATH_BY_LABEL,
-            element_maker=NO_NS_TEI_E
+            element_maker=NO_NS_TEI_E,
+            default_tei_filename_suffix=(
+                ReferenceSegmenterTeiTrainingDataGenerator.DEFAULT_TEI_FILENAME_SUFFIX
+            ),
+            default_data_filename_suffix=(
+                ReferenceSegmenterTeiTrainingDataGenerator.DEFAULT_DATA_FILENAME_SUFFIX
+            )
         )
