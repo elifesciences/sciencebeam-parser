e1a70ad33a9f7eeadeda5b2164639399bff5cc1a
Daniel Ecer <de-code@users.noreply.github.com>
2022-01-15 20:37:05 +0000

    generate delft data for single file

--- sciencebeam_parser/training/cli/generate_delft_data.py
+++ sciencebeam_parser/training/cli/generate_delft_data.py
@@ -3,6 +3,19 @@ import logging
 from pathlib import Path
 from typing import List, Optional
 
+from lxml import etree
+
+import numpy as np
+
+from sciencebeam_trainer_delft.sequence_labelling.tag_formatter import (
+    TagOutputFormats,
+    format_tag_result,
+    get_tag_result
+)
+
+from sciencebeam_parser.utils.xml import get_text_content
+from sciencebeam_parser.utils.tokenizer import get_tokenized_tokens
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -40,15 +53,40 @@ def parse_args(argv: Optional[List[str]] = None) -> argparse.Namespace:
 
 
 def generate_delft_training_data(
+    tei_source_path: str,
     delft_output_path: str
 ):
+    LOGGER.debug('tei_source_path: %r', tei_source_path)
+    tei_root = etree.parse(tei_source_path).getroot()
+    token_texts = np.asarray(
+        [get_tokenized_tokens(get_text_content(tei_root))],
+        dtype='object'
+    )
+    token_labels = np.asarray(
+        [['O'] * len(token_texts[0])],
+        dtype='object'
+    )
+    features = np.full(shape=(1, len(token_texts[0]), 1), fill_value='0')
+    tag_result = get_tag_result(texts=token_texts, labels=token_labels)
+    LOGGER.debug('tag_result: %r', tag_result)
+    LOGGER.debug('features: %r', features)
     LOGGER.debug('delft_output_path: %r', delft_output_path)
-    Path(delft_output_path).write_text('', encoding='utf-8')
+    Path(delft_output_path).parent.mkdir(parents=True, exist_ok=True)
+    Path(delft_output_path).write_text(
+        format_tag_result(
+            tag_result=tag_result,
+            output_format=TagOutputFormats.DATA,
+            texts=token_texts,
+            features=features
+        ),
+        encoding='utf-8'
+    )
 
 
 def run(args: argparse.Namespace):
     LOGGER.info('args: %r', args)
     generate_delft_training_data(
+        tei_source_path=args.tei_source_path,
         delft_output_path=args.delft_output_path
     )
 
--- tests/training/cli/generate_delft_data_test.py
+++ tests/training/cli/generate_delft_data_test.py
@@ -1,6 +1,13 @@
+# pylint: disable=not-callable
 import logging
 from pathlib import Path
 
+from lxml import etree
+from lxml.builder import E
+
+from sciencebeam_trainer_delft.sequence_labelling.reader import (
+    load_data_and_labels_crf_file
+)
 from sciencebeam_parser.training.cli.generate_delft_data import (
     main
 )
@@ -17,6 +24,11 @@ MINIMAL_EXAMPLE_PDF_PATTERN = 'test-data/minimal-example*.pdf'
 SOURCE_FILENAME_1 = 'test1.pdf'
 
 
+TOKEN_1 = 'token1'
+TOKEN_2 = 'token2'
+TOKEN_3 = 'token3'
+
+
 @log_on_exception
 class TestMain:
     def test_should_be_able_to_generate_segmentation_training_data(
@@ -26,6 +38,15 @@ class TestMain:
         tei_source_path = tmp_path / 'tei'
         raw_source_path = tmp_path / 'raw'
         output_path = tmp_path / 'output.data'
+        tei_source_path.write_bytes(etree.tostring(
+            E('tei', E('text', *[
+                TOKEN_1,
+                ' ',
+                TOKEN_2,
+                ' ',
+                TOKEN_3
+            ]))
+        ))
         main([
             '--model-name=segmentation',
             f'--tei-source-path={tei_source_path}',
@@ -33,3 +54,10 @@ class TestMain:
             f'--delft-output-path={output_path}'
         ])
         assert output_path.exists()
+        texts, _labels, _features = load_data_and_labels_crf_file(
+            str(output_path)
+        )
+        LOGGER.debug('texts: %r', texts)
+        assert len(texts) == 1
+        assert list(texts[0]) == [TOKEN_1, TOKEN_2, TOKEN_3]
+        assert list(_labels[0]) == ['O'] * 3
