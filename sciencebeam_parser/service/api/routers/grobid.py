f1f12c6a7ac7ee59f9e3ca5561daeea9fc210a3b
Daniel Ecer <de-code@users.noreply.github.com>
2025-12-09 18:29:27 +0000

    added /processReferences endpoint

--- sciencebeam_parser/service/api/routers/grobid.py
+++ sciencebeam_parser/service/api/routers/grobid.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from fastapi.responses import FileResponse
 
 from sciencebeam_parser.app.parser import ScienceBeamParserSessionSource
-from sciencebeam_parser.processors.fulltext.config import FullTextProcessorConfig
+from sciencebeam_parser.processors.fulltext.config import FullTextProcessorConfig, RequestFieldNames
 from sciencebeam_parser.service.api.dependencies import (
     assert_and_get_first_accept_matching_media_type_factory,
     get_sciencebeam_parser_session_source_dependency_factory
@@ -108,6 +108,39 @@ def create_grobid_router(
             Depends(
                 get_sciencebeam_parser_session_source_dependency_factory(
                     fulltext_processor_config=fulltext_processor_config
+                )
+            )
+        ],
+        response_media_type: Annotated[
+            str,
+            Depends(
+                assert_and_get_first_accept_matching_media_type_factory(
+                    [MediaTypes.TEI_XML, MediaTypes.JATS_XML]
+                )
+            )
+        ],
+    ) -> FileResponse:
+        return get_processed_source_to_response_media_type(
+            source=source,
+            response_media_type=response_media_type
+        )
+
+    @router.post(
+        '/processReferences',
+        response_class=FileResponse,
+        responses={
+            200: {"content": TEI_AND_JATS_XML_CONTENT_DOC},
+            406: {"description": "No acceptable media type"},
+        },
+    )
+    def process_references_api(
+        source: Annotated[
+            ScienceBeamParserSessionSource,
+            Depends(
+                get_sciencebeam_parser_session_source_dependency_factory(
+                    fulltext_processor_config=fulltext_processor_config.get_for_requested_field_names({
+                        RequestFieldNames.REFERENCES
+                    })
                 )
             )
         ],
