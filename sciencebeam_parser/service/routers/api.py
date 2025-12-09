ce1c65fffaefb993802e72581e62c91e7da34325
Daniel Ecer <de-code@users.noreply.github.com>
2025-12-09 16:55:55 +0000

    Demonstrate dependency injection

--- sciencebeam_parser/service/routers/api.py
+++ sciencebeam_parser/service/routers/api.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Optional
+from typing import Annotated, Iterator, Optional, Protocol
 
 from fastapi import (
     Body,
@@ -7,19 +7,32 @@ from fastapi import (
     FastAPI,
     File,
     HTTPException,
+    Request,
     UploadFile,
     status
 )
 
 
 from sciencebeam_parser.app.parser import (
-    ScienceBeamParser
+    ScienceBeamParser,
+    ScienceBeamParserSession,
+    ScienceBeamParserSessionSource
 )
 from sciencebeam_parser.utils.data_wrapper import MediaDataWrapper
 from sciencebeam_parser.utils.media_types import MediaTypes
 
 
 LOGGER = logging.getLogger(__name__)
+
+
+class ScienceBeamParserSessionDependencyFactory(Protocol):
+    def __call__(self, *args, **kwargs) -> Iterator[ScienceBeamParserSession]:
+        pass
+
+
+class ScienceBeamParserSessionSourceDependencyFactory(Protocol):
+    def __call__(self, *args, **kwargs) -> Iterator[ScienceBeamParserSessionSource]:
+        pass
 
 
 def get_media_data_wrapper_for_upload_file(
@@ -62,10 +75,49 @@ def resolve_media_data(
     )
 
 
+def get_sciencebeam_parser(request: Request) -> ScienceBeamParser:
+    return request.app.state.sciencebeam_parser
+
+
+def get_sciencebeam_parser_session_dependency_factory(
+    **session_kwargs,
+) -> ScienceBeamParserSessionDependencyFactory:
+    def get_session(
+        *,
+        sciencebeam_parser: ScienceBeamParser = Depends(get_sciencebeam_parser)
+    ) -> Iterator[ScienceBeamParserSession]:
+        with sciencebeam_parser.get_new_session(**session_kwargs) as session:
+            yield session
+
+    return get_session
+
+
+def get_sciencebeam_parser_session_source_dependency_factory(
+    **session_kwargs,
+) -> ScienceBeamParserSessionSourceDependencyFactory:
+    def get_source(
+        *,
+        session: ScienceBeamParserSession = Depends(
+            get_sciencebeam_parser_session_dependency_factory(**session_kwargs)
+        ),
+        data_wrapper: MediaDataWrapper = Depends(resolve_media_data),
+    ) -> Iterator[ScienceBeamParserSessionSource]:
+        source_path = session.temp_path / "source.file"
+        source_path.write_bytes(data_wrapper.data)
+
+        yield session.get_source(
+            source_path=str(source_path),
+            source_media_type=data_wrapper.media_type,
+        )
+
+    return get_source
+
+
 def create_api_app(
-    sciencebeam_parser: ScienceBeamParser  # pylint: disable=unused-argument
+    sciencebeam_parser: ScienceBeamParser
 ) -> FastAPI:
     app = FastAPI()
+    app.state.sciencebeam_parser = sciencebeam_parser
 
     @app.get('/')
     def api_root() -> dict:
@@ -79,5 +131,17 @@ def create_api_app(
     ):
         LOGGER.info('file: %r, input: %r', media.filename, media.media_type)
         return f'test: {media.filename}, {media.media_type}'
+
+    @app.post("/process2")
+    def process2(
+        source: Annotated[
+            ScienceBeamParserSessionSource,
+            Depends(
+                get_sciencebeam_parser_session_source_dependency_factory()
+            )
+        ]
+    ):
+        LOGGER.info('file: %r, input: %r', source.source_path, source.source_media_type)
+        return f'test: {source.source_path}, {source.source_media_type}'
 
     return app
